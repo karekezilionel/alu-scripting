5507545799cc2f34f1eaf559a5d28e9e10c3318b
karekezilionel <karekezilionel@gmail.com>
2025-06-24 12:29:07 -0700

    done

---
+++ 0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers to a subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "linux:subreddit.counter:v1.0 (by /u/yourname)"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except Exception:
+        return 0
