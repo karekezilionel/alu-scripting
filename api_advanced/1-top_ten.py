6d59ef18415cc0e1635659f1154e88cd001f1060
karekezilionel <l.karekezi@alustudent.com>
2025-07-14 03:18:12 -0700

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts in a subreddit."""
 
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    params = {'limit': 10}
 
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+        if response.status_code != 200:
+            print(None)
+            return
+
         json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+        posts = json_data.get('data', {}).get('children', [])
+
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            title = post.get('data', {}).get('title')
+            print(title)
+
+    except Exception:
         print(None)
