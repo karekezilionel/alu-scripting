549892b4555ffc9e16cd74d71c2f0d6c490f0240
karekezilionel <l.karekezi@alustudent.com>
2025-07-14 03:23:45 -0700

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-This module queries the Reddit API and prints
-the titles of the first 10 hot posts for a given subreddit.
-"""
+""" Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts in a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    params = {'limit': 10}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-        if response.status_code != 200:
-            print(None)
-            return
-
+    if response.status_code == 200:
         json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-
-        if not posts:
-            print(None)
-            return
-
-        for post in posts:
-            title = post.get('data', {}).get('title')
-            print(title)
-
-    except Exception:
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
