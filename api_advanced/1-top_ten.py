a6c3dab8f090455cb7ecd99b9b92a165e4739fbb
karekezilionel <l.karekezi@alustudent.com>
2025-07-14 03:07:06 -0700

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""Fetches and prints the titles of the first 10 hot posts for a subreddit."""
+""" Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts in a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 10}
 
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
-        return
-
-    try:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-
-        if not posts:
-            print(None)
-            return
-
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-    except Exception:
-        print(None)
-
